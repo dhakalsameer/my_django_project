125624261402d588e084208e1abef43b1725b8a2
Sameer Dhakal <sameerdhakal1234@gmail.com>
2026-01-13 13:49:41 +0545

    Working with the modules in the django app.

---
+++ bms/pustak/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+    book_name = models.CharField(max_length=100)
+    book_description = models.TextField()
+    book_image = models.ImageField(upload_to ='book_images/')
+    def __str__(self):
+        return self.book_name
