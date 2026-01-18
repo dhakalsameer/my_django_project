e59c428ae5c5070fb7b70aa3bf0fe6f14ae442d3
Sameer Dhakal <sameerdhakal1234@gmail.com>
2026-01-18 08:52:29 +0545

    Creating the books view.

--- bms/pustak/views.py
+++ bms/pustak/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from .models import Book
 
 # Create your views here.
+def book(request):
+    if request.method == 'POST':
+        bm = request.POST.get('book_name')
+        bd = request.POST.get('book_description')
+        bi = request.FILES['book_image']
+        Book.objects.create(book_name=bm, book_description=bd, book_image=bi)
+        return redirect('/')
+    queryset = Book.objects.all()
+    context = {'books': queryset}
+    return render(request,'pustak/books.html', context)
